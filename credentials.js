ae4297feb937946f08b7289abf156c8056020565
Andreas Krühlmann <kruhlmann@protonmail.com>
2018-05-17 17:01:19 +0200

    added dummy credentials file

---
+++ credentials.js
@@ -0,0 +1,8 @@
+module.exports = {
+    host: "sql_host_address",
+    user: "sql_user",
+    database: "sql_database",
+    password: "sql_user_password",
+    oauth: "twitch_oauth",
+    username: "bot_username"
+}
