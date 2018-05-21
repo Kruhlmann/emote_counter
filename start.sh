8bc933039644fc4b006ba80382cef0f71d9a236e
Kruhlmann <kruhlmann@protonmail.com>
2018-05-20 18:47:32 -0700

    progress?

--- start.sh
+++ start.sh
@@ -2,7 +2,22 @@
 echo "Updating emote database please wait..."
 # Download global twitch emotes
 curl -o "global_emotes.json" "https://twitchemotes.com/api_cache/v3/global.json"
-# Add further API requirements here
+# Extract MySQL credentials from credentials.json
+mysql_password=`grep -o 'password: *"[^"]*"' credentials.js | grep -o '"[^"]*"$'`
+mysql_user=`grep -o 'user: *"[^"]*"' credentials.js | grep -o '"[^"]*"$'`
+mysql_database=`grep -o 'database: *"[^"]*"' credentials.js | grep -o '"[^"]*"$'`
+mysql_host=`grep -o 'host: *"[^"]*"' credentials.js | grep -o '"[^"]*"$'`
+# Check if any fields are empty
+if [ -z "$mysql_password" ] || [ -z "$mysql_user" ] || [ -z "$mysql_database" ] || [ -z "$mysql_host" ]
+then
+    echo "Error: please fill in the credentials.js file"
+    exit 3
+fi
+
+sudo mysql -u $mysql_user -p"$mysql_password" -D $mysql_database -h 127.0.0.1
+
+
 
 # Start the bot
+exit 1
 node main.js
