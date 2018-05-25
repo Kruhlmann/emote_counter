e5ad008ced630ff2d4df054a5dc967351af539a6
Andreas Krühlmann <kruhlmann@protonmail.com>
2018-05-25 13:46:51 +0200

    adjusted credentials template now that I'm using twitch kraken API

--- credentials.js
+++ credentials.js
@@ -4,5 +4,7 @@ module.exports = {
     database: "sql_database",
     password: "sql_user_password",
     oauth: "twitch_oauth",
-    username: "bot_username"
+    username: "bot_username",
+    client_id: "twitch_client_id",
+    client_secret: "twitch_client_secret"
 }
